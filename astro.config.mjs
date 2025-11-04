31675ee141cc1a2a93fa8999e91cea27db3d104b
Biswajit Charan <biswajitcharan@192.168.1.5>
2025-11-04 20:53:11 +0530

    updated code

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+import sanity from '@sanity/astro';
+
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [
+    sanity({
+      projectId: 'wfqtcpjl',
+      dataset: 'production',
+      // Set useCdn to false if you're building statically.
+      useCdn: false,
+      studioBasePath: '/admin',
+    }),
+    ,
+    react(),
+  ],
+  output: 'server',
+});
