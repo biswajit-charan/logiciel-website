301b213244ecbc9a35267c118bfe6a8fc03d6329
Biswajit Charan <biswajitcharan@192.168.1.5>
2025-11-05 18:48:42 +0530

    updated code

---
+++ schemaTypes/posts.ts
@@ -0,0 +1,89 @@
+import type { Rule } from "sanity";
+
+const categories = [
+    { title: "daily digest", value: "daily digest" },
+    { title: "ai tools", value: "ai tools" },
+];
+
+export const posts = {
+    name: "post",
+    title: "Post",
+    type: "document",
+
+    fields: [
+        {
+            name: "name",
+            title: "Name",
+            type: "string",
+            validation: (Rule: Rule) => Rule.required(),
+        },
+
+        {
+            name: "slug",
+            title: "Slug",
+            type: "slug",
+            options: { source: "name" },
+        },
+
+        {
+            name: "author",
+            title: "Author",
+            type: "string",
+            validation: (Rule: Rule) => Rule.required(),
+        },
+
+        {
+            name: "category",
+            title: "Category",
+            type: "string",
+            options: {
+                list: categories,
+            },
+            initialValue: "daily digest",
+        },
+
+        {
+            name: "tags",
+            title: "Tags",
+            type: "array",
+            of: [{ type: "string" }],
+            options: {
+                layout: "tags",
+            },
+        },
+
+        {
+            name: "date",
+            title: "Date",
+            type: "datetime",
+        },
+
+        {
+            name: "featured",
+            title: "Featured",
+            type: "boolean",
+            initialValue: false,
+        },
+
+        {
+            name: "image",
+            title: "Image",
+            type: "image",
+            options: { hotspot: true },
+            fields: [
+                {
+                    name: "alt",
+                    title: "Alt",
+                    type: "string",
+                },
+            ],
+        },
+
+        {
+            name: "content",
+            title: "Content",
+            type: "array",
+            of: [{ type: "block" }],
+        },
+    ],
+};
